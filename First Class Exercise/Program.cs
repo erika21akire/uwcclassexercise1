b504165985967a4e4042b9124f81b8f0f671377d
erika21akire <5100162@mypillar7uwc.acza>
2023-11-28 11:55:50 +0200

    First Commit

---
+++ First Class Exercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace First_Class_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string reversedString = Reverse(Console.ReadLine()); 
+            //Console.WriteLine("Output: " + reversedString);
+            bool forwardBackwardResult = IsSameForwardsAndBackwards(Console.ReadLine());
+        }
+
+        static bool IsSameForwardsAndBackwards(string FrontBackText)
+        {
+            string reverse = String.Empty;
+            for (int i = FrontBackText.Length - 1; i > -1; i--)
+            { reverse += FrontBackText[i]; }
+            if (FrontBackText == reverse)
+            { Console.WriteLine("True"); }
+            else { Console.WriteLine("False"); }
+            return false;
+        }
+    }
+}
